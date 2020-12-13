36794fcb4c281dc061ac3c43321e603f55ae4f82
ykGoose <72850129+ykGoose@users.noreply.github.com>
2020-12-13 15:46:31 +0900

    _

--- TrafficLightsSUI/ColorCircleView.swift
+++ TrafficLightsSUI/ColorCircleView.swift
@@ -14,9 +14,9 @@ struct ColorCircleView: View {
     var body: some View {
         Color(color)
             .opacity(status)
-                .clipShape(Circle())
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                .overlay(Circle().stroke(Color.gray, lineWidth: 4))    }
+            .clipShape(Circle())
+            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.gray, lineWidth: 4))    }
 }
 
 struct ColorCircleView_Previews: PreviewProvider {
