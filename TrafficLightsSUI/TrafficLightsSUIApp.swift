c23076c4e1a328d13c9e96ab3e1c9229b14395b8
ykGoose <72850129+ykGoose@users.noreply.github.com>
2020-12-13 10:38:17 +0900

    Initial Commit

---
+++ TrafficLightsSUI/TrafficLightsSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsSUIApp.swift
+//  TrafficLightsSUI
+//
+//  Created by Владимир Падусов on 13.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
